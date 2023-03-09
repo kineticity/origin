cb78e131958edb3f23fb7d9fe282be9d61224f9c
kineticity <90329679+kineticity@users.noreply.github.com>
2023-03-10 01:23:39 +0530

    Update addnumbers.java

--- addnumbers.java
+++ addnumbers.java
@@ -2,7 +2,7 @@ public class addnumbers
 {  
 public static void main(String args[])   
 {  
-int n1 = 225, n2 = 115, sum;  
+int n1 = 20, n2 = 100, sum;  
 sum = n1 + n2;  
 System.out.println("The sum of numbers is: "+sum);  
 System.out.println("Hello, World!");
